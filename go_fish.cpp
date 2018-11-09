3c3f8c0205eafc76ece4044865de39fc97a3abf7
Iris Ham <iris.ham@utexas.edu>
2018-11-09 13:50:13 -0600

    ok

--- go_fish.cpp
+++ go_fish.cpp
@@ -1,21 +1,46 @@
+// FILE: card_demo.cpp
+// This is a small demonstration program showing how the Card and Deck classes are used.
+#include <iostream>    // Provides cout and cin
+#include <cstdlib>     // Provides EXIT_SUCCESS
 #include "card.h"
-#include "deck.h"
 #include "player.h"
-#include <cstdlib>
-#include <iostream>
-#include <stdlib.h>
-#include <string>
-#include <sstream>
-#include <vector>
-#include <time.h>
+#include "deck.h"
 
-int main(){
-    Player one;
-    Player two;
-    Player three;
+using namespace std;
+
+
+// PROTOTYPES for functions used by this demonstration program:
+void dealHand(Deck &d, Player &p, int numCards);
 
 
 
 
-
+int main( )
+{
+    int numCards = 5;
+    
+    Player p1("Joe");
+    Player p2("Jane");
+    
+    Deck d;  //create a deck of cards
+    d.shuffle();
+    
+    dealHand(d, p1, numCards);
+    dealHand(d, p2, numCards);
+       
+    cout << p1.getName() <<" has : " << p1.showHand() << endl;
+    cout << p2.getName() <<" has : " << p2.showHand() << endl;
+    
+    return EXIT_SUCCESS;  
 }
+
+
+
+void dealHand(Deck &d, Player &p, int numCards)
+{
+   for (int i=0; i < numCards; i++)
+      p.addCard(d.dealCard());
+}
+   
+
+
